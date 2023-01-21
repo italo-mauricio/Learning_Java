53cd5bd44c9b9ff49eb13fbda7325af6852b6b73
italo <italogithub@gmail.com>
2023-01-21 14:50:29 -0300

    reforçando o aprendizado com operadores

--- Curso Java/src/Operadores.java
+++ Curso Java/src/Operadores.java
@@ -6,8 +6,19 @@
 public class Operadores {
     public static void main(String[] args) {
         int num1 = 10;
-        int num2 = 2;
-        int soma = num1 / num2;
-        System.out.println(soma);
+        long numeroLong = 200000L;
+
+        double numeroDouble = 10d;
+        float numeroFloat = 20f;
+        numeroFloat = (float) numeroDouble;
+        float restoDiv = (21%2);
+        //System.out.println("Resto da divisão "+ (21%2));
+        if (restoDiv == 0){
+            System.out.println("O numero é par");
+
+        }else {
+            System.out.println("O numero é impar");
+        }
+        System.out.println(numeroFloat);
     }
 }
