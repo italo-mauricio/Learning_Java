23c2d47c993aa96cf893a71ad2b40edffb3ccd56
italo <italogithub@gmail.com>
2023-01-21 13:56:14 -0300

    operadores

---
+++ Curso Java/src/Operadores.java
@@ -0,0 +1,13 @@
+/***
+ * Operadores básicos do java
+ */
+
+
+public class Operadores {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 2;
+        int soma = num1 / num2;
+        System.out.println(soma);
+    }
+}
