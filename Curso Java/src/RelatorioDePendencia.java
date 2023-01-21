602f5b10b388f74dec331be3bfbe9a96dca74a84
italo <italogithub@gmail.com>
2023-01-21 13:30:45 -0300

    update

--- Curso Java/src/RelatorioDePendencia.java
+++ Curso Java/src/RelatorioDePendencia.java
@@ -3,7 +3,8 @@ public class RelatorioDePendencia {
         String nome = "italo";
         String endereco = "Rua tal de tal";
         String telefone = "4002-8022";
-
+        System.out.println("Sistema de Hotelaria\n" +
+                "Verificação de pendencia de cliente\n");
         System.out.println("Usuario: "+nome+"\nEndereco: "+endereco+"\nTelefone: "+telefone+
                 "\nNão possui nenhum vínculo de pendencia");
     }
