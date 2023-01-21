2e043c4b45775da2339d0c0c82fbf91d6a2c6308
italo <italogithub@gmail.com>
2023-01-21 11:34:22 -0300

    aula sobre tipos primitivos

---
+++ Curso Java/src/Primitivos.java
@@ -0,0 +1,14 @@
+/***
+ * Comentário de documentação oficial Java
+ *
+ * São 8 tipos primivitos
+ *
+ * int, float, long, double, char, short, boolean, byte
+ * @param args parâmetro de entrada do método main
+ */
+
+public class Primitivos {
+    public static void main(String[] args) {
+        System.out.println("ola");
+    }
+}
