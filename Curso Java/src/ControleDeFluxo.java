d3a100dcd0de385231706207c823fcae110805b0
italo <italogithub@gmail.com>
2023-01-22 10:55:34 -0300

    update

--- Curso Java/src/ControleDeFluxo.java
+++ Curso Java/src/ControleDeFluxo.java
@@ -1,12 +1,14 @@
 public class ControleDeFluxo {
     public static void main(String[] args) {
-        int idade = 20;
-        float salario = 2800;
-        boolean c = false;
-        if (c = true) {
-        System.out.println("Entrou no clube");
-    }else{
-            System.out.println("Não entrou no clube");
+        int idade = 19;
+
+        if (idade < 15) {
+        System.out.println("Categoria infantil");
+    }else if (idade >= 15 && idade < 18){
+            System.out.println("Categoria juvenil");
+
+        }else {
+            System.out.println("Adulto");
         }
     }
 }
