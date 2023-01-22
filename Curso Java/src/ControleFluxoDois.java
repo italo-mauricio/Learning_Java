d3f45715dcf97cd18698fcc0703d12270cfbed96
italo <italogithub@gmail.com>
2023-01-22 17:15:08 -0300

    treinando conversões

---
+++ Curso Java/src/ControleFluxoDois.java
@@ -0,0 +1,5 @@
+public class ControleFluxoDois {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Curso Java/src/ConvereGaloesEmLitros.java
@@ -0,0 +1,11 @@
+public class ConvereGaloesEmLitros {
+    public static void main(String[] args) {
+        double gallons;
+        double liters;
+
+        gallons = 10;
+
+        liters = gallons * 3.7854;
+        System.out.println(gallons + " gallons is " + liters + " liters.");
+    }
+}
