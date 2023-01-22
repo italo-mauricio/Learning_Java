0f56c808161f59add1ce41dbf26787da73fba1c8
italo <italogithub@gmail.com>
2023-01-22 10:47:19 -0300

    condicionais

--- Curso Java/src/ControleDeFluxo.java
+++ Curso Java/src/ControleDeFluxo.java
@@ -1,5 +1,12 @@
 public class ControleDeFluxo {
     public static void main(String[] args) {
-
+        int idade = 20;
+        float salario = 2800;
+        boolean c = false;
+        if (c = true) {
+        System.out.println("Entrou no clube");
+    }else{
+            System.out.println("Não entrou no clube");
+        }
     }
 }
