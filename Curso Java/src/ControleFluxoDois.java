2a911a42c09dafd732056adfc075db0a5d59102e
italo <italogithub@gmail.com>
2023-01-23 13:37:36 -0300

    utilizando operador ternário

--- Curso Java/src/ControleFluxoDois.java
+++ Curso Java/src/ControleFluxoDois.java
@@ -1,5 +1,17 @@
+/***
+ * Operador ternário
+ *
+ * Não deve-se utilizar operador ternário quando temos duas ou mais instruções
+ */
+
+
+
 public class ControleFluxoDois {
     public static void main(String[] args) {
-        
+        int idade = 20;
+        String status;
+
+        status = idade < 18 ? "Não adulto" : "Adulto";
+        System.out.println(status);
     }
 }
