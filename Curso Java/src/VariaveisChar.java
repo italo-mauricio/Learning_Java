862a0800eb7530a434871edd21d023507d98dbe1
italo <italogithub@gmail.com>
2023-01-26 14:29:25 -0300

    trabalhando com unicode

---
+++ Curso Java/src/VariaveisChar.java
@@ -0,0 +1,9 @@
+public class VariaveisChar {
+    public static void main(String[] args) {
+        char o = 111;
+        char i = 105;
+        char interrogacao = 0X00E1;
+
+        System.out.println(""+o+i+interrogacao);
+    }
+}
