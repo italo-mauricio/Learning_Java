7cc890069e1448916a36d4f4ba21730accf37d76
italo <italogithub@gmail.com>
2023-01-27 13:24:50 -0300

    aprendendo operadores aritméticos

---
+++ Curso Java/src/OperadoresAritmeticos.java
@@ -0,0 +1,26 @@
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+        int resultado = 1 + 2;
+        System.out.println(resultado);
+
+        resultado = resultado - 1;
+        System.out.println(resultado);
+
+        resultado = resultado * 2;
+        System.out.println(resultado);
+
+        resultado = resultado / 2;
+        System.out.println(resultado);
+
+        resultado = resultado + 8;
+        System.out.println(resultado);
+
+        resultado = resultado % 7;
+        System.out.println(resultado);
+
+        String primeiroNome = "Esta é ";
+        String segundoNome = "uma string concatenada";
+        String terceiroNome = primeiroNome + segundoNome;
+        System.out.println(terceiroNome);
+    }
+}
