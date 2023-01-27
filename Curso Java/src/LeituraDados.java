56011939332cc55766a0c50b2ea53a03380e80c9
italo <italogithub@gmail.com>
2023-01-27 11:49:13 -0300

    update

--- Curso Java/src/LeituraDados.java
+++ Curso Java/src/LeituraDados.java
@@ -5,8 +5,16 @@ public class LeituraDados {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Digite seu nome completo: ");
+        System.out.print("Digite seu nome completo: ");
         String nomeCompleto = scan.nextLine();
         System.out.println("Seu nome completo é: " + nomeCompleto);
+
+        System.out.print("Digite seu primeiro nome: ");
+        String primeiroNome = scan.next();
+        System.out.println("Seu primeiro nome é: " + primeiroNome);
+
+        System.out.print("Digite a sua idade: ");
+        int idade = scan.nextInt();
+        System.out.println("Você tem " + idade + " anos de idade");
     }
 }
