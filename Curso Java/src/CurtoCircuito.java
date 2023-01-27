ef7b02891f6fe8a72e8f37492adfa466d4219334
italo <italogithub@gmail.com>
2023-01-27 16:45:07 -0300

    estudando curto circuito

---
+++ Curso Java/src/CurtoCircuito.java
@@ -0,0 +1,12 @@
+public class CurtoCircuito {
+    public static void main(String[] args) {
+        boolean verdadeiro = true;
+        boolean falso = false;
+        boolean resultado1 = falso &
+                verdadeiro; // aqui o java vai verificar toda a expressão mesmo sabendo que é falsa
+        boolean resultado2 = falso &&
+                verdadeiro; // aqui ele já vai pular direto no inicio do falso
+        System.out.println(resultado1);
+        System.out.println(resultado2);
+    }
+}
--- Curso Java/src/OperadoresLogicos.java
+++ Curso Java/src/OperadoresLogicos.java
@@ -6,5 +6,15 @@ public class OperadoresLogicos {
         boolean resultado2 = (valor1 == 1) || (valor2 != 2);
         System.out.println("Valor1 é 1 AND valor2 é 2 - resultado: " + resultado1);
         System.out.println("Valor1 é 1 OR valor2 é 2 - resultado: " + resultado2);
+
+        boolean verdadeiro = true;
+        boolean falso = false;
+
+        System.out.println(verdadeiro && falso);
+        System.out.println(verdadeiro || falso);
+        System.out.println(verdadeiro ^ falso);
+        System.out.println(!verdadeiro && falso);
+        System.out.println(!verdadeiro);
+        System.out.println(!falso);
     }
 }
