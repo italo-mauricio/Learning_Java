fa43313d0653facdfbc0de376d10f6f1e49e7ff5
italo <italogithub@gmail.com>
2023-01-27 18:43:18 -0300

    adicionando exercicios

---
+++ Curso Java/src/Exercicios/Exercicio1.java
@@ -0,0 +1,14 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Digite um número ");
+        int num = scan.nextInt();
+
+        System.out.println("O número informado foi: " + num);
+
+    }
+}
