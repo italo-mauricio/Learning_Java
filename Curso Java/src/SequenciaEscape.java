e460fa3d9d89981ef880b0711d7d082cc1ad9ef0
italo <italogithub@gmail.com>
2023-01-26 23:04:08 -0300

    update

--- Curso Java/src/SequenciaEscape.java
+++ Curso Java/src/SequenciaEscape.java
@@ -2,5 +2,6 @@ public class SequenciaEscape {
     public static void main(String[] args) {
         //"Hello, World"
         System.out.println("\"Hello, world!\"");   // colocando aspas duplas no output
-    }
+        System.out.println("1\\4"); // para representar a barra invertida no número
+     }
 }
