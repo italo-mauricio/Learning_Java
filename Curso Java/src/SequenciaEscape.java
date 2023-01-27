8a65e8005c24957c9740c522d2311d3ff5bbd3bc
italo <italogithub@gmail.com>
2023-01-26 23:01:33 -0300

    sequencia de escape

---
+++ Curso Java/src/SequenciaEscape.java
@@ -0,0 +1,6 @@
+public class SequenciaEscape {
+    public static void main(String[] args) {
+        //"Hello, World"
+        System.out.println("\"Hello, world!\"");   // colocando aspas duplas no output
+    }
+}
