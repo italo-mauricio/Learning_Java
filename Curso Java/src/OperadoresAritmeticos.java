bb6073fcc6231660558a8661cd796c7aa1157327
italo <italogithub@gmail.com>
2023-01-27 13:53:40 -0300

    update

--- Curso Java/src/OperadoresAritmeticos.java
+++ Curso Java/src/OperadoresAritmeticos.java
@@ -22,5 +22,28 @@ public class OperadoresAritmeticos {
         String segundoNome = "uma string concatenada";
         String terceiroNome = primeiroNome + segundoNome;
         System.out.println(terceiroNome);
+
+        System.out.println(resultado++);
+        // mesma coisa que:
+        //System.out.println(resultado);
+        //qresultado = resultado + 1;
+        //resultado += 1;
+        System.out.println(++resultado);
+        // mesma coisa que:
+        //resultado += 1;
+        //System.out.println(resultado);
+
+        resultado --;
+        System.out.println(resultado);
+
+        System.out.println(resultado--);
+        System.out.println(--resultado);
+        int num = 5;
+        num++;
+        System.out.println(num);
+        System.out.println(++num);
+        num--;
+        System.out.println(num+=1);
+
     }
 }
