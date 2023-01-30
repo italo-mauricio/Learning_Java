1f5ba70214863b92a271324da8b8db77679b9681
italo <italogithub@gmail.com>
2023-01-29 23:02:24 -0300

    update

--- Curso_Java/src/Aulas/CondicionaisIfElse.java
+++ Curso_Java/src/Aulas/CondicionaisIfElse.java
@@ -11,8 +11,21 @@ public class CondicionaisIfElse {
 
         if (idade >= 18){
             System.out.println("É maior de idade, você tem: " + idade + " anos");
+            System.out.println("Você tem idade válida para comprar!");
+            System.out.print("Digite o valor do item: ");
+            double valor = scan.nextDouble();
+            if (valor <= 10){
+                System.out.println("Está muito barato, pode comprar!");
+            }else if (valor > 10 && valor < 15){
+                System.out.println("Você pode pedir um desconto!");
+            }else if (valor >= 15 && valor < 17){
+                System.out.println("Você pode pesquisar mais!");
+            }else if (valor > 17){
+                System.out.println("Está muito caro!");
+            }
         }else{
             System.out.println("É menor de idade, sua idade é de: " + idade + " anos");
         }
+
     }
 }
