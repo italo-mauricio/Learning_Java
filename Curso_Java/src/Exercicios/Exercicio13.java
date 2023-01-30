98e84a480aa451cbc9c1aa11336d7d0591061315
italo <italogithub@gmail.com>
2023-01-29 22:08:28 -0300

    adicionando exercicio 13

--- Curso_Java/src/Exercicios/Exercicio13.java
+++ Curso_Java/src/Exercicios/Exercicio13.java
@@ -1,7 +1,19 @@
 package Exercicios;
 
+import java.util.Scanner;
+
 public class Exercicio13 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        System.out.print("Digite o tamanho do arquivo: ");
+        double tamanhoArquivo = scan.nextDouble();
+
+        System.out.print("Digite a velocidade da internet: ");
+        double velocidadeNet = scan.nextDouble();
+
+        double tempo = tamanhoArquivo / velocidadeNet;
+
+        System.out.println("Tempo de DOWNLOAD é: " + tempo);
     }
 }
