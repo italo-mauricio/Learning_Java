28ca6e65d2957605747367bd6a18a7924c1b0d56
italo <italogithub@gmail.com>
2023-01-31 15:39:50 -0300

    adicionando condicional de contagem de caracteres

--- Curso_Java/src/Exercicios/Exercicio20.java
+++ Curso_Java/src/Exercicios/Exercicio20.java
@@ -11,29 +11,38 @@ public class Exercicio20 {
         System.out.println("Digite uma letra: ");
         String letra = scan.next();
 // primeira forma com IF ELSE
-        if (letra.equalsIgnoreCase("a") ||
-                letra.equalsIgnoreCase("e") || letra.equalsIgnoreCase("i") ||
-                letra.equalsIgnoreCase("o") || letra.equalsIgnoreCase("u")){
-            System.out.println("Vogal");
-        }else {
-            System.out.println("Consoante");
+
+        if (letra.length() > 1){
+            System.out.println("Impossivel continuar, você digitou mais de uma letra");
+        }else{
+            if (letra.equalsIgnoreCase("a") ||
+                    letra.equalsIgnoreCase("e") || letra.equalsIgnoreCase("i") ||
+                    letra.equalsIgnoreCase("o") || letra.equalsIgnoreCase("u")){
+                System.out.println("Vogal");
+            }else {
+                System.out.println("Consoante");
+            }
         }
 // segunda forma com SWTICH CASE
-        switch (letra){
-            case "a":
-            case "e":
-            case "i":
-            case "o":
-            case "u":
-            case "A":
-            case "E":
-            case "I":
-            case "O":
-            case "U":
-                System.out.println("Vogal");
-                break;
-            default:
-                System.out.println("Consoante");
+        if (letra.length() > 1){
+            System.out.println("Impossível continuar, você digitou mais de uma letra");
+        }else {
+            switch (letra) {
+                case "a":
+                case "e":
+                case "i":
+                case "o":
+                case "u":
+                case "A":
+                case "E":
+                case "I":
+                case "O":
+                case "U":
+                    System.out.println("Vogal");
+                    break;
+                default:
+                    System.out.println("Consoante");
+            }
         }
     }
 }
