18f7b9d6e72cc86a430a7a9ffc511da6c9f3e6fd
italo <italogithub@gmail.com>
2023-01-31 14:40:49 -0300

    adicionando exercicio 15

---
+++ Curso_Java/src/Exercicios/Exercicio15.java
@@ -0,0 +1,22 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+/***
+ * Faça um programa que peça um número e diga se ele é positivo ou negativo
+ */
+
+public class Exercicio15 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int num = scan.nextInt();
+
+        if (num < 0){
+            System.out.println("O número é negativo, sendo ele: " + num);
+        }else{
+            System.out.println("O número é positivo, sendo ele: " + num);
+        }
+    }
+}
