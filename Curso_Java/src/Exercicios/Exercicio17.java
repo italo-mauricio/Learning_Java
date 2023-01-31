d90b626dfcd8357545038a267671467d82a5ffed
italo <italogithub@gmail.com>
2023-01-31 14:58:29 -0300

    adicionando exercicio 17

---
+++ Curso_Java/src/Exercicios/Exercicio17.java
@@ -0,0 +1,21 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+/***
+ * Faça um programa que verifique se a letra digitada é maiuscula ou minuscula
+ */
+public class Exercicio17 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite uma letra: ");
+        String usuario = scan.next();
+
+        if (usuario.equals(usuario.toUpperCase())){
+            System.out.println("Vc digitou uma letra MAIUSCULA");
+        }else{
+            System.out.println("Você digitou uma letra MINUSCULA");
+        }
+    }
+}
