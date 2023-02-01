389ad4d3271005121f79ef29ea755a6c54e50beb
italo <italogithub@gmail.com>
2023-01-31 23:35:06 -0300

    adicionando exercicio 25

---
+++ Curso_Java/src/Exercicios/Exercicio25.java
@@ -0,0 +1,35 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+/***
+ * Faça um programa que peça os 3 lados de um triângulo. O programa deverá informar
+ * se os valores podem ser um triângulo, indique caso os lados formem um triângulo, se o mesmo é:
+ * equilátero, isóceles ou escaleno.
+ */
+public class Exercicio25 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite o lado 1: ");
+        int lado1 = scan.nextInt();
+        System.out.print("Digite o lado 2: ");
+        int lado2 = scan.nextInt();
+        System.out.print("Digite o lado 3: ");
+        int lado3 = scan.nextInt();
+
+        if (((lado1 + lado2) > lado3) ||
+                ((lado1 + lado3) > lado2) ||
+                ((lado2 + lado3) > lado1)){
+            if (lado1 == lado2 && lado1 == lado3){
+                System.out.println("Triângulo Equilátero");
+            }else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3){
+                System.out.println("Triângulo Isóceles");
+            }else {
+                System.out.println("Triângulo Escaleno");
+            }
+        }else {
+            System.out.println("Não forma um triângulo");
+        }
+    }
+}
