86df2a9a16e4736aa961376965c6beb5a4b04f2a
italo <italogithub@gmail.com>
2023-02-01 14:58:43 -0300

    adicionando exercicio 27

---
+++ Curso_Java/src/Exercicios/Exercicio27.java
@@ -0,0 +1,19 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio27 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite o ano do seu nascimento: ");
+        int ano = scan.nextInt();
+
+        if ((ano % 400 == 0)|| (ano % 4 == 0 && ano % 100 != 0)){
+            System.out.println("Você nasceu no ano: " + ano + "\nO ano é bissexto");
+        }else {
+            System.out.println("Você nasceu no ano: " + ano + "\nO ano não é bissexto");
+        }
+
+    }
+}
