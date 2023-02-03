a2e0e1629938a33a0cc70fe1274ffb5efcd37694
italo <italogithub@gmail.com>
2023-02-03 14:20:09 -0300

    update

--- Curso_Java/src/Aulas/LoopFor.java
+++ Curso_Java/src/Aulas/LoopFor.java
@@ -2,11 +2,16 @@ package Aulas;
 
 public class LoopFor {
     public static void main(String[] args) {
+        // loop de repetição normal
         for (int i=0;i<10;i++){
             System.out.println("I tem o valor de: " + i);
         }
         for (int i = 5; i > 0; i--){
             System.out.println("I tem o valor de: " + i);
+        }
+        // loop de repetição com mais de uma variável
+        for (int i = 0, j = 10; i < j; i++, j-- ){
+            System.out.println("I tem o valor de: " + i + " J tem o valor de: " + j);
         }
     }
 }
