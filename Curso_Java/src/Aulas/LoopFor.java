c28e9366034516c98708634b868f7c58a2e32a33
italo <italogithub@gmail.com>
2023-02-03 14:15:46 -0300

    adicionando aula sobre for

---
+++ Curso_Java/src/Aulas/LoopFor.java
@@ -0,0 +1,12 @@
+package Aulas;
+
+public class LoopFor {
+    public static void main(String[] args) {
+        for (int i=0;i<10;i++){
+            System.out.println("I tem o valor de: " + i);
+        }
+        for (int i = 5; i > 0; i--){
+            System.out.println("I tem o valor de: " + i);
+        }
+    }
+}
