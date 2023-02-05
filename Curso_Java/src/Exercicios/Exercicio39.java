8fbd6324d20832fb11a9ce18493e2d477898e26d
italo <italogithub@gmail.com>
2023-02-05 01:52:47 -0300

    adicionando exercicio 39

---
+++ Curso_Java/src/Exercicios/Exercicio39.java
@@ -0,0 +1,26 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+/***
+ * faça um programa que pela um número 5 vezes, some e tire a média
+ */
+
+public class Exercicio39 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double media;
+        int num;
+        int soma = 0;
+
+        for (int i=0; i<5; i++){
+            System.out.print("Digite um número: ");
+            num = scan.nextInt();
+            soma += num;
+        }
+        media = soma / 5;
+
+        System.out.println("Soma: " + soma);
+        System.out.println("Media: " + media);
+    }
+}
