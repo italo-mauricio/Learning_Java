972a4cb15120568589af1635ab1ec4f3aaf3b6ab
italo <italogithub@gmail.com>
2023-02-05 13:54:53 -0300

    adicionando exercicio fibonacci

---
+++ Curso_Java/out/production/Learning_Java/README.md
@@ -0,0 +1,1 @@
+# Repositório destinado ao aprendizado de java através do curso da loiane no youtube
--- Curso_Java/src/Exercicios/Exercicio46.java
+++ Curso_Java/src/Exercicios/Exercicio46.java
@@ -1,7 +1,34 @@
 package Curso_Java.src.Exercicios;
 
+import java.util.Scanner;
+
+/***
+ * Fibonacci
+ */
 public class Exercicio46 {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite o enésimo termo da série Fibonacci: ");
+        int n = scan.nextInt();
+
+        int primeiroNum = 1;
+        int segundoNum = 1;
+        int aux;
+
+        System.out.println(primeiroNum);
+        System.out.println(segundoNum);
+
+        for (int i = 3; i<=n; i++){
+
+            aux = primeiroNum + segundoNum;
+            primeiroNum = segundoNum;
+            segundoNum = aux;
+
+            System.out.println(aux);
+
+        }
 
     }
 }
