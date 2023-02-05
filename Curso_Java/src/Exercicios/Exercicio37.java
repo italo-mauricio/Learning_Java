7d714f4b7489920e73cb9df332153ee8a4830023
italo <italogithub@gmail.com>
2023-02-04 23:15:04 -0300

    adicionando exercicio 37

---
+++ Curso_Java/src/Exercicios/Exercicio37.java
@@ -0,0 +1,14 @@
+package Exercicios;
+
+/***
+ * faça um programa que imprima os numeros de 1 a 20 tanto na vertical como na horizontal
+ */
+public class Exercicio37 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 20; i++){
+            System.out.println("Valor de i: " + i);
+            System.out.print(i + " ");
+        }
+    }
+}
