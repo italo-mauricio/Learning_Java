6ef7e7eb86b37954bbc574691b03e5b177804d88
italo <italogithub@gmail.com>
2023-02-06 09:48:13 -0300

    fazendo correções

--- Curso_Java/src/Exercicios/Exercicio55.java
+++ Curso_Java/src/Exercicios/Exercicio55.java
@@ -10,6 +10,7 @@ public class Exercicio55 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         //System.out.print("Lojas Tabajara");
+
         boolean sair = false;
         int qtdProdutos;
         String continuarCompra;
@@ -28,21 +29,30 @@ public class Exercicio55 {
                 qtdProdutos = scan.nextInt();
                 total = 0;
 
-                for (int i = 1; i <= qtdProdutos;i++){
+
+                for (int i = 1; i <= qtdProdutos; i++){
                     System.out.println("Informe o preço do produto: " + i);
                     preco = scan.nextDouble();
-                    total+=preco;
                     output += "Produto " + i + ": R$ " + preco + "\n";
+                    total += preco;
                 }
                 output += "Total: R$ " + total;
                 System.out.println("Total: R$ " + total);
                 System.out.println("Digite o valor pago: ");
                 valorPago = scan.nextDouble();
-                output += "\nDinheiro: R$ " + valorPago + "\n";
-                troco = total - valorPago;
-                output += "Troco: R$ " + troco;
-                System.out.println(output);
+
+                if (valorPago < total){
+                    System.out.println("Ainda faltam: " + (valorPago - total));
+                    System.out.println("Por favor, pague o valor correto do produto!");
+
+                }else{
+                    output += "\nDinheiro: R$ " + valorPago + "\n";
+                    troco = total - valorPago;
+                    output += "Troco: R$ " + troco;
+                    System.out.println(output);
+                }
             }else{
+                System.out.println("Obrigado, volte sempre!");
                 sair = true;
             }
         }while (!sair);
