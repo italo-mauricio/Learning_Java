3114b1ce2e84ed1c9d63ac8f35bb2883891275d4
italo <italogithub@gmail.com>
2023-02-07 14:33:33 -0300

    adicionando aula sobre break e continue

---
+++ Curso_Java/src/Aulas/CondicionalSaidaBreak.java
@@ -0,0 +1,12 @@
+package Curso_Java.src.Aulas;
+
+import java.util.Scanner;
+
+public class CondicionalSaidaBreak {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+        int num = scan.nextInt();
+    }
+}
