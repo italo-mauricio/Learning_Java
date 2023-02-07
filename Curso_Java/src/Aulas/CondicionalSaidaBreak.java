386b95a59284a70ce27b0b36948a5dc38d600541
italo <italogithub@gmail.com>
2023-02-07 14:40:12 -0300

    update

--- Curso_Java/src/Aulas/CondicionalSaidaBreak.java
+++ Curso_Java/src/Aulas/CondicionalSaidaBreak.java
@@ -6,7 +6,16 @@ public class CondicionalSaidaBreak {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Digite um número: ");
+        System.out.print("Digite um número: ");
         int num = scan.nextInt();
+        System.out.print("Digite um limite: ");
+        int maximo = scan.nextInt();
+
+        for (int i = num; i <= maximo; i++){
+            if (i % 7 == 0){
+                System.out.println("O valor de i é: " + i);
+                break;
+            }
+        }
     }
 }
