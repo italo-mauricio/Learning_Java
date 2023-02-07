fcddedfde3cc5cb6620ec29354f9e28a04989982
italo <italogithub@gmail.com>
2023-02-07 13:39:09 -0300

    adicionando a soma dos N termos

--- Curso_Java/src/Exercicios/Exercicio62.java
+++ Curso_Java/src/Exercicios/Exercicio62.java
@@ -13,10 +13,12 @@ public class Exercicio62 {
         Scanner scan = new Scanner(System.in);
         System.out.println("Digite o valor de N: ");
         int n = scan.nextInt();
+        double soma = 0;
 
         for (int i = 1, j = 1; i <= n; i++, j+=2){
             System.out.print(i + "/" + j + " + ");
-
+            soma += i/j;
         }
+        System.out.println("Soma: " + soma);
     }
 }
---
+++ Curso_Java/src/Exercicios/Exercicio63.java
@@ -0,0 +1,7 @@
+package Curso_Java.src.Exercicios;
+
+public class Exercicio63 {
+    public static void main(String[] args) {
+
+    }
+}
