af1e4a27043f14690686cd5c8e3ac239eeb6bf99
italo <italogithub@gmail.com>
2023-02-07 02:06:17 -0300

    up

---
+++ Curso_Java/src/Exercicios/Exercicio62.java
@@ -0,0 +1,17 @@
+package Curso_Java.src.Exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio62 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite o valor de N: ");
+        int n = scan.nextInt();
+
+        for (int i = 1; i <= n; i++){
+
+        }
+
+        
+    }
+}
