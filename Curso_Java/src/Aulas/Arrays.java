cc9474b12fbeec54d128db44e74e147e742328e7
italo <italogithub@gmail.com>
2023-02-08 14:39:45 -0300

    trabalhando com arrays

--- Curso_Java/src/Aulas/Arrays.java
+++ Curso_Java/src/Aulas/Arrays.java
@@ -3,6 +3,11 @@ package Curso_Java.src.Aulas;
 public class Arrays {
     public static void main(String[] args) {
         double[] temperaturas = new double[365];     // <--- boa prática ||indicando o número de posições
+        temperaturas[0] = 31.3;
+        temperaturas[1] = 31.6;
+        temperaturas[2] = 32.0;
+        temperaturas[3] = 32.5;
+        System.out.println(temperaturas[2]);
 
     }
 
