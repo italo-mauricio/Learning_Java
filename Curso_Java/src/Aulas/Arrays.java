bcb6162074629dc43d9181136d5aa9243e25d4ff
italo <italogithub@gmail.com>
2023-02-08 14:53:44 -0300

    utilizando o for melhorado

--- Curso_Java/src/Aulas/Arrays.java
+++ Curso_Java/src/Aulas/Arrays.java
@@ -8,6 +8,17 @@ public class Arrays {
         temperaturas[2] = 32.0;
         temperaturas[3] = 32.5;
         System.out.println(temperaturas[2]);
+        System.out.println("O tamanho do array: " + temperaturas.length);
+        System.out.println("O valor da temperatura do dia 3 é: " + temperaturas[2]);
+
+        for (int i=0; i<temperaturas.length; i++){
+            System.out.println("O valor da temperatura do dia " + (i+1) + " é: " + temperaturas[i]);
+        }
+
+        // for melhorado eu consigo acessar os valores do array diretamente
+        for (double temp : temperaturas){
+            System.out.println(temp);
+        }
 
     }
 
