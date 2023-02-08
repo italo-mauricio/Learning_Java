7cea67ead0763baba7729ecba377f45c36c929eb
italo <italogithub@gmail.com>
2023-02-08 01:14:25 -0300

    estudando arrays

---
+++ Curso_Java/src/Aulas/Arrays.java
@@ -0,0 +1,11 @@
+package Curso_Java.src.Aulas;
+
+public class Arrays {
+    public static void main(String[] args) {
+        double tempDiaria001 = 31.3;
+        double tempDiaria002 = 32;
+        double tempDiaria003 = 33.7;
+        double tempDiaria004 = 34;
+        double tempDiaria005 = 33.1;
+    }
+}
