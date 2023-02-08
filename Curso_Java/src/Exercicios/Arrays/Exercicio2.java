7d7d83981cda32fd8bec5d861289eba566030303
italo <italogithub@gmail.com>
2023-02-08 17:00:33 -0300

    feat: adicionando exercicio 2 de Arrays

--- Curso_Java/src/Exercicios/Arrays/Exercicio2.java
+++ Curso_Java/src/Exercicios/Arrays/Exercicio2.java
@@ -3,26 +3,23 @@ package Curso_Java.src.Exercicios.Arrays;
 import java.util.Scanner;
 
 /***
- * 1 - Criar um vetor A com 5 elementos inteiros. Construir um vetor B de mesmo tipo
- * e tamanho e com os "mesmos" elementos do Vetor A, ou seja, B[i] = A[i];
+ * 2 - Crie um vetor A com 8 elementos inteiros. Construa um vetor B de mesmo tipo e tamanho e com elementos do vetor A
+ * multiplicados por 2, ou seja B[i] = A[i] * 2
  */
 
-public class Exercicio1 {
+public class Exercicio2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int[] vetorA = new int[5];
+        int[] vetorA = new int[8];
         int[] vetorB = new int[vetorA.length];
 
         for (int i = 0; i < vetorA.length; i++){
             System.out.println("Digite o valor da posição: " + i);
             vetorA[i] = scan.nextInt();
 
-            vetorB[i] = vetorA[i];
+            vetorB[i] = vetorA[i] * 2;
         }
-        //for (int i = 0; i < vetorA.length; i++){
-        //    vetorB[i] = vetorA[i];
-        //}
 
         System.out.print("Vetor A: ");
         for (int i = 0; i < vetorA.length; i++){
@@ -35,6 +32,7 @@ public class Exercicio1 {
             System.out.print(vetorB[i] + " ");
         }
         System.out.println();
+
 
     }
 }
