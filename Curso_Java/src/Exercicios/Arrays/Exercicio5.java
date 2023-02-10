17989998e5056ab506da67c3a2c4cb1fccbb01fa
italo <italogithub@gmail.com>
2023-02-10 03:25:12 -0300

    adicionando exercicio 5

---
+++ Curso_Java/src/Exercicios/Arrays/Exercicio5.java
@@ -0,0 +1,33 @@
+package Curso_Java.src.Exercicios.Arrays;
+
+import java.util.Scanner;
+
+public class Exercicio5 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int[] vetorA = new int[15];
+        int[] vetorB = new int[vetorA.length];
+
+        for (int i = 0; i < vetorA.length; i++){
+            System.out.println("Digite o valor da posição: " + i);
+            vetorA[i] = scan.nextInt();
+
+            vetorB[i] = (int) Math.sqrt(vetorA[i]);  // podemos "forçar" a tipagem, ou apenas mudar o tipo na declaração da variável
+        }
+
+        System.out.print("Vetor A: ");
+        for (int i = 0; i < vetorA.length; i++){
+            System.out.print(vetorA[i] + " ");
+        }
+        System.out.println();
+
+        System.out.print("Vetor B: ");
+        for (int i = 0; i < vetorB.length; i++){
+            System.out.print(vetorB[i] + " ");
+        }
+        System.out.println();
+
+    }
+}
