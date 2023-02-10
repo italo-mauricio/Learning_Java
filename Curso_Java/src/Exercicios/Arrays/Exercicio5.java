a84542d809e4a8c523ef76b2366289fce40a48f5
italo <italogithub@gmail.com>
2023-02-10 03:51:40 -0300

    update

--- Curso_Java/src/Exercicios/Arrays/Exercicio5.java
+++ Curso_Java/src/Exercicios/Arrays/Exercicio5.java
@@ -1,5 +1,6 @@
 package Curso_Java.src.Exercicios.Arrays;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Exercicio5 {
@@ -7,25 +8,27 @@ public class Exercicio5 {
 
         Scanner scan = new Scanner(System.in);
 
-        int[] vetorA = new int[15];
-        int[] vetorB = new int[vetorA.length];
+        int[] vetorA = new int[4];
+        double[] vetorB = new double[vetorA.length];
 
         for (int i = 0; i < vetorA.length; i++){
             System.out.println("Digite o valor da posição: " + i);
             vetorA[i] = scan.nextInt();
 
-            vetorB[i] = (int) Math.sqrt(vetorA[i]);  // podemos "forçar" a tipagem, ou apenas mudar o tipo na declaração da variável
+            vetorB[i] = Math.sqrt(vetorA[i]);
         }
+
+        DecimalFormat df = new DecimalFormat("#,###.###");  // talvez não funciona no java atual
 
         System.out.print("Vetor A: ");
         for (int i = 0; i < vetorA.length; i++){
-            System.out.print(vetorA[i] + " ");
+           System.out.print(vetorA[i] + " ");
         }
         System.out.println();
 
         System.out.print("Vetor B: ");
         for (int i = 0; i < vetorB.length; i++){
-            System.out.print(vetorB[i] + " ");
+            System.out.print(df.format(vetorB[i] + " "));
         }
         System.out.println();
 
