86cc0c696eccc098d8d8f9256682021f5bf7442d
italo <italogithub@gmail.com>
2023-02-12 21:37:23 -0300

    add arrays: exercicio 27

---
+++ Curso_Java/src/Exercicios/Arrays/Exercicio27.java
@@ -0,0 +1,11 @@
+package Curso_Java.src.Exercicios.Arrays;
+
+/***
+ * Criar dois vetores A e B cada um com 10 elementos inteiros. Construir um vetor C de mesmo tipo e tamanho.
+ */
+
+public class Exercicio27 {
+    public static void main(String[] args) {
+
+    }
+}
