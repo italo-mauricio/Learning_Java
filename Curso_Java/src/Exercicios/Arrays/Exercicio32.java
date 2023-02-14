6ca31f44e3a97b116eb787b6a84010d3808bf542
italo <italogithub@gmail.com>
2023-02-14 06:23:46 -0300

    add arrays: exercicio 32

---
+++ Curso_Java/src/Exercicios/Arrays/Exercicio32.java
@@ -0,0 +1,13 @@
+package Curso_Java.src.Exercicios.Arrays;
+
+
+/***
+ * Ler um vetor A com 20 elementos. Separar os elementos pares e ímpares utilizando apenas um vetor extra B.
+ * Sugestão: no inicio do vetor B armazene os elementos pares de  A e nas posições restantes do vetor B armazene
+ * os elementos de A que são ímpares
+ */
+public class Exercicio32 {
+    public static void main(String[] args) {
+
+    }
+}
