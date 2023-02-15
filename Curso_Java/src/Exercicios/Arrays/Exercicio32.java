dc88c0c37d504fb031a9ad7baad3c973e11d687a
italo <italogithub@gmail.com>
2023-02-15 19:50:06 -0300

    update

--- Curso_Java/src/Exercicios/Arrays/Exercicio32.java
+++ Curso_Java/src/Exercicios/Arrays/Exercicio32.java
@@ -1,6 +1,8 @@
 package Curso_Java.src.Exercicios.Arrays;
 
 
+import java.util.Scanner;
+
 /***
  * Ler um vetor A com 20 elementos. Separar os elementos pares e ímpares utilizando apenas um vetor extra B.
  * Sugestão: no inicio do vetor B armazene os elementos pares de  A e nas posições restantes do vetor B armazene
@@ -8,6 +10,25 @@ package Curso_Java.src.Exercicios.Arrays;
  */
 public class Exercicio32 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] vetorA = new int[20];
+        int[] vetorB = new int[vetorA.length]; // pares e ímpares
+
+        int posB = 0;
+        int posC = 0;
+
+        for (int i = 0; i < vetorA.length; i++){
+            System.out.println("Digite o valor da posição: " + i);
+            vetorA[i] = scan.nextInt();
+        }
+
+        for (int i = 0; i < vetorA.length; i++){
+            if (vetorA[i] % 2 == 0){
+
+            }
+        }
+
+
 
     }
 }
