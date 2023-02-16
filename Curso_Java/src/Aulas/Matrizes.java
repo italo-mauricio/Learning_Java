cbbd84de2c8461a8ead96552756911c2a7bdafa2
italo <italogithub@gmail.com>
2023-02-16 02:11:45 -0300

    add aprendizado de matrizes

---
+++ Curso_Java/src/Aulas/Matrizes.java
@@ -0,0 +1,31 @@
+package Curso_Java.src.Aulas;
+
+public class Matrizes {
+    public static void main(String[] args) {
+
+        double[][] notasAlunos = new double[3][4];
+
+        notasAlunos[0][0] = 10;
+        notasAlunos[0][1] = 7;
+        notasAlunos[0][2] = 8;
+        notasAlunos[0][3] = 5;
+
+        notasAlunos[1][0] = 9;
+        notasAlunos[1][1] = 7;
+        notasAlunos[1][2] = 5;
+        notasAlunos[1][3] = 9;
+
+        notasAlunos[2][0] = 9;
+        notasAlunos[2][1] = 7;
+        notasAlunos[2][2] = 4;
+        notasAlunos[2][3] = 8;
+
+        for (int i = 0; i < notasAlunos.length; i++){
+             System.out.println(notasAlunos[i] + " ");
+            for(int j = 0; j < notasAlunos.length; j++){
+                System.out.println(notasAlunos[j] + " ");
+            }
+        }
+
+    }
+}
