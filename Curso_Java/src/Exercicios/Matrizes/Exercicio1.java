9a006309ab490bfa291575a9a6d56e7ab5c98d00
italo <italogithub@gmail.com>
2023-02-16 02:41:48 -0300

    add matrizes: exercicio 1

---
+++ Curso_Java/src/Exercicios/Matrizes/Exercicio1.java
@@ -0,0 +1,15 @@
+package Curso_Java.src.Exercicios.Matrizes;
+
+
+/**
+ * Faça um programa que crie uma matriz 3x3x3 onde cada elemento da matriz seja igual a
+ * soma dos seus índices (exemplo: M[1,2,1] = 1+2+1=4)
+ * Obtenha a soma de todos os elementos da matriz, e obtenha soma dos elementos cujos valores
+ * são pares e a soma dos elementos cujos valores são ímpares.
+ * Exibir na tela os valores da soma total, soma dos pares e soma dos ímpares.
+ */
+public class Exercicio1 {
+    public static void main(String[] args) {
+
+    }
+}
