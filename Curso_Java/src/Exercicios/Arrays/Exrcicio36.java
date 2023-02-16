82085f3551ca45d717281050e6d22ef0e316222e
italo <italogithub@gmail.com>
2023-02-16 01:01:03 -0300

    add arrays: exercicio 36

---
+++ Curso_Java/src/Exercicios/Arrays/Exrcicio36.java
@@ -0,0 +1,34 @@
+package Curso_Java.src.Exercicios.Arrays;
+
+
+import java.util.Scanner;
+
+/***
+ * Criar um vetor A com 10 elementos inteiros. Escreva um programa que imprima cada elemento do vetor A e a relação de todos
+ * divisores do respectivo elemento.
+ */
+public class Exrcicio36 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] vetorA = new int[5];
+
+
+        for (int i = 0; i < vetorA.length; i++){
+            System.out.println("Digite o valor da posição: " + i);
+            vetorA[i] = scan.nextInt();
+        }
+
+        for (int i = 0; i < vetorA.length; i++){
+            System.out.println("Analizando o número: " + vetorA[i]);
+
+            for (int j = 1; j < vetorA[i]; j++){
+                if (vetorA[i] % j ==0){
+                    System.out.println(j + " é divisor");
+                }else{
+                    System.out.println(j + " Não é divisor");
+                }
+            }
+        }
+
+    }
+}
