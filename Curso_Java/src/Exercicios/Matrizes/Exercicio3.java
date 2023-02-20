da36c7b42777d30c160356bb63c0f1c786f0dc8f
italo <italogithub@gmail.com>
2023-02-20 18:42:25 -0300

    add matrizes: exercicio 3

--- Curso_Java/src/Exercicios/Matrizes/Exercicio3.java
+++ Curso_Java/src/Exercicios/Matrizes/Exercicio3.java
@@ -1,2 +1,14 @@
-package Curso_Java.src.Exercicios.Matrizes;public class Exercicio3 {
+package Curso_Java.src.Exercicios.Matrizes;
+
+/***
+ * Gere e imprima uma matirz M 4x4 com valores aleatórios entre 0 e 9. Após isso
+ * determine o maior número da matriz e a sua posição (linha/coluna)
+ */
+
+
+
+public class Exercicio3 {
+    public static void main(String[] args) {
+
+    }
 }
