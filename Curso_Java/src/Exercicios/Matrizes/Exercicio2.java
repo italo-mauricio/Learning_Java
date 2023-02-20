f57197b63332929d610e025e1f010f22f90de60e
italo <italogithub@gmail.com>
2023-02-20 17:57:49 -0300

    ajeitando

--- Curso_Java/src/Exercicios/Matrizes/Exercicio2.java
+++ Curso_Java/src/Exercicios/Matrizes/Exercicio2.java
@@ -15,7 +15,7 @@ public class Exercicio2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Digite o número de pessoas que serão entrevistadas: ");
+        System.out.print("Digite o número de pessoas que serão entrevistadas: ");
         int entrevistados = scan.nextInt();
 
         String[][] nomesFilhos = new String[entrevistados][];
@@ -29,8 +29,6 @@ public class Exercicio2 {
             for (int j = 0; j < nomesFilhos[i].length; j++){
                 System.out.println("Digite o nome do filho: " + (j+1));
                 nomesFilhos[i][j] = scan.next();
-
-
             }
         }
 
@@ -41,8 +39,5 @@ public class Exercicio2 {
             }
 
         }
-
-
-
     }
 }
