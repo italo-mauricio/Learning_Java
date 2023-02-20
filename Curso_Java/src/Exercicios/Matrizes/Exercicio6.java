df5da84fd702b16f3b8f2b35f4a30f3189be8d16
italo <italogithub@gmail.com>
2023-02-20 20:33:53 -0300

    add matrizes: exercicio 6

---
+++ Curso_Java/src/Exercicios/Matrizes/Exercicio6.java
@@ -0,0 +1,17 @@
+package Curso_Java.src.Exercicios.Matrizes;
+
+
+/***
+ * Faça um programa para armazenar em uma matriz os compromissos de uma agenda pessoal.
+ * Cada dia do mês deve conhter 24 horas, onde para cada uma destas 24 horas podemos associar
+ * uma tarefa específica (compromisso agendado). O programa deve ter um menu onde o usuário indica o dia do mês
+ * que deseja alterar e a hora; entrando em seguida com o compromisso, ou então, o usuário pode também
+ * consultar a agenda, fornecendo o dia e a hora para obter o compromisso agendado.
+ *
+ */
+
+public class Exercicio6 {
+    public static void main(String[] args) {
+
+    }
+}
