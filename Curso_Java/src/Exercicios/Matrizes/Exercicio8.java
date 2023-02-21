3d34be3578afea85970c049172e1cdd85606c3e8
italo <italogithub@gmail.com>
2023-02-21 19:07:32 -0300

    add matrizes: jogo da velha

---
+++ Curso_Java/src/Exercicios/Matrizes/Exercicio8.java
@@ -0,0 +1,14 @@
+package Curso_Java.src.Exercicios.Matrizes;
+
+/***
+ * Faça um programa para jogar o jogo da velha. O programa deve permitir que dois jogadores façam uma partida
+ * do jogo da velha usando o computador para ver o tabuleiro. Cada jogador vai alternadamente informando a posição onde deseja
+ * colocar a sua peça ('o' e 'X'). O programa deve impedir jogadas inválidas e determinar automaticamente quando o jogo terminou e quem foi o
+ * vencedor (jogador 1 ou jogador 2). A cada nova jogada, o programa deve atualizar a situação do tabuleiro na tela.
+ */
+
+public class Exercicio8 {
+    public static void main(String[] args) {
+
+    }
+}
