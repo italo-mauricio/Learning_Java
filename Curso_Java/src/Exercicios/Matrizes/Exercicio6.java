7edc663aa59969d49ba76abef32a21b977f23286
italo <italogithub@gmail.com>
2023-02-21 18:11:56 -0300

    resolvi

--- Curso_Java/src/Exercicios/Matrizes/Exercicio6.java
+++ Curso_Java/src/Exercicios/Matrizes/Exercicio6.java
@@ -17,77 +17,58 @@ public class Exercicio6 {
         Scanner scan = new Scanner(System.in);
 
         String[][] compromissos = new String[31][24];
-
-        boolean sair = false;
-        byte opcao;
-        boolean diaValido = false;
-        boolean horaValida = false;
         int dia = 0;
         int hora = 0;
+        boolean sair = false;
+        int opcao;
+        while (!sair){
 
-        while(!sair){
-            System.out.println("Digite 1 para adicionar compromisso");
-            System.out.println("Digite 2 para verificar compromisso");
-            System.out.println("Digite 0 para sair");
-            opcao = scan.nextByte();
+            System.out.println("Digite 1 para adicionar compromisso.");
+            System.out.println("Digite 2 para verificar compromisso.");
+            System.out.println("Digite 0 para sair.");
 
-            if (opcao == 1){
+            opcao = scan.nextInt();
 
+            if (opcao == 1){ //adicionar compromisso
+
+
+                boolean diaValido = false;
                 while (!diaValido){
-                    System.out.println("Digite o dia do mês: ");
+                    System.out.println("Entre com o dia do mês");
                     dia = scan.nextInt();
                     if (dia > 0 && dia <= 31){
                         diaValido = true;
-
-                    }else{
-                        System.out.println("Dia inválido, digite novamente!");
+                    } else {
+                        System.out.println("Dia inválido, digite novamente");
                     }
                 }
+
+                boolean horaValida = false;
+
                 while (!horaValida){
-                    System.out.println("Digite o horário do compromisso: ");
+                    System.out.println("Entre com a hora do compromisso");
                     hora = scan.nextInt();
                     if (hora >= 0 && hora <= 24){
                         horaValida = true;
-
-                    }else{
-                        System.out.println("Hora inválida, digite novamente!");
+                    } else {
+                        System.out.println("Hora inválida, digite novamente");
                     }
                 }
-                dia--;  // retirar 1 dia para bater as 31 posições
-                System.out.println("Digite o compromisso: ");
+
+                dia--;
+                System.out.println("Digite o compromisso");
                 compromissos[dia][hora] = scan.next();
 
-            }else if (opcao == 2){
-
-                while (!diaValido){
-                    System.out.println("Digite o dia do mês: ");
-                    dia = scan.nextInt();
-                    if (dia > 0 && dia <= 31){
-                        diaValido = true;
-                    }else{
-                        System.out.println("Dia inválido, digite novamente!");
-                    }
-                }
-                while (!horaValida){
-                    System.out.println("Digite o horário do compromisso: ");
-                    hora = scan.nextInt();
-                    if (hora >= 0 && hora <= 24){
-                        horaValida = true;
-                    }else{
-                        System.out.println("Hora inválida, digite novamente!");
-                    }
-                }
-                dia--;
+            } else if (opcao == 2){ //verificar compromisso
                 System.out.println("O compromisso agendado é:");
-                System.out.println(compromissos[dia][hora]);
+                System.out.println(compromissos[dia][hora] + " ");
 
-            }else if (opcao == 0){
-                System.out.println("Obrigado, volte sempre!");
+            } else if (opcao == 0){
                 sair = true;
-            }else {
-                System.out.println("Opção inválida!");
+            } else {
+                System.out.println("Opção inválida, digite novamente");
             }
-
         }
+
     }
 }
