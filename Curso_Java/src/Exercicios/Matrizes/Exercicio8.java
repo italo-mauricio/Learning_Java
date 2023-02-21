49b006c1cd2243ec8ddfedecf962dc84f818b926
italo <italogithub@gmail.com>
2023-02-21 19:16:25 -0300

    update game

--- Curso_Java/src/Exercicios/Matrizes/Exercicio8.java
+++ Curso_Java/src/Exercicios/Matrizes/Exercicio8.java
@@ -1,5 +1,7 @@
 package Curso_Java.src.Exercicios.Matrizes;
 
+import java.util.Scanner;
+
 /***
  * Faça um programa para jogar o jogo da velha. O programa deve permitir que dois jogadores façam uma partida
  * do jogo da velha usando o computador para ver o tabuleiro. Cada jogador vai alternadamente informando a posição onde deseja
@@ -9,6 +11,40 @@ package Curso_Java.src.Exercicios.Matrizes;
 
 public class Exercicio8 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        char[][] jogoVelha = new char[3][3];
+
+        System.out.println("Jogador 1 = X");
+        System.out.println("Jogador 2 = O");
+
+        boolean ganhou = false;
+        int jogada = 1;
+        char sinal;
+        int linha = 0;
+        int coluna = 0;
+
+        while(!ganhou){
+
+            if (jogada % 2 == 1){  // jogador 1
+
+                System.out.println("Vez do jogador 1. Escolha linha e coluna (1 - 3 )");
+                sinal = 'X';
+            } else {
+                System.out.println("Vez do jogador 2. Escolha linha e coluna (1 - 3 )");
+                sinal = 'O';
+            }
+
+            boolean linhaValida = false;
+
+            while(!linhaValida){
+                System.out.println("Digite a linha (1 / 2 / 3) ");
+
+
+            }
+
+
+
+        }
     }
 }
