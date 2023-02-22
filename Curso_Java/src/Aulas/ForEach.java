ad50774e425f660cd1f26603bf92f6333fb0a857
italo <italogithub@gmail.com>
2023-02-22 05:50:21 -0300

    exemplo com arrays multidimensionais

--- Curso_Java/src/Aulas/ForEach.java
+++ Curso_Java/src/Aulas/ForEach.java
@@ -18,7 +18,34 @@ public class ForEach {
         // usando for each
         System.out.println("Utilizando o for each: ");
         for(int nota : notas){
-            System.out.println(nota);
+            System.out.print(nota);
+        }
+
+        // exemplo 2
+        System.out.println("Exemplo com Arrays multidimensionais");
+        double[][] notasAlunos = new double[3][4];
+
+
+        notasAlunos[0][0] = 10;
+        notasAlunos[0][1] = 7;
+        notasAlunos[0][2] = 8;
+        notasAlunos[0][3] = 5;
+
+        notasAlunos[1][0] = 9;
+        notasAlunos[1][1] = 7;
+        notasAlunos[1][2] = 5;
+        notasAlunos[1][3] = 9;
+
+        notasAlunos[2][0] = 9;
+        notasAlunos[2][1] = 7;
+        notasAlunos[2][2] = 4;
+        notasAlunos[2][3] = 8;
+
+        for (double[] notasAluno : notasAlunos){
+                for(double nota : notasAluno){
+                    System.out.print(nota + " ");
+                }
+            System.out.println();
         }
     }
 }
