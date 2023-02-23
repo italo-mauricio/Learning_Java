b7147e1ece73ef1ac6c5e75acc3a2a98e0dad2b1
italo <italogithub@gmail.com>
2023-02-23 05:16:56 -0300

    app lampada

---
+++ POO/Exercicios/Exercicio1/Lampada.java
@@ -0,0 +1,12 @@
+package POO.Exercicios.Exercicio1;
+
+public class Lampada {
+    String modelo;
+    String tensao;
+    int potencia;
+    String corLuz;
+    String cor;
+    int garantiaMeses;
+    String [] tipos;
+    boolean tipoAbajur;
+}
