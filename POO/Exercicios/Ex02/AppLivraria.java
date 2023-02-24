ae70b36ada4d17a9789c52dfff8344e9aa065656
italo <italogithub@gmail.com>
2023-02-24 08:49:44 -0300

    usando valueof para inserir uma String em um valor booleano

--- POO/Exercicios/Ex02/AppLivraria.java
+++ POO/Exercicios/Ex02/AppLivraria.java
@@ -1,6 +1,8 @@
 package POO.Exercicios.Ex02;
 
 
+import java.util.Date;
+
 public class AppLivraria {
     String nomeLivro;
     String autor;
@@ -8,6 +10,9 @@ public class AppLivraria {
     int anoLancamento;
     String isbn;
     double precoLivro;
+    boolean emprestado;
+    Date dataEntrega;
+    String emprestadoA;
 
     public static void main(String[] args) {
         AppLivraria LivroLivraria = new AppLivraria();
@@ -15,10 +20,22 @@ public class AppLivraria {
         LivroLivraria.precoLivro = 15.0;
         LivroLivraria.autor = "Pedro";
         LivroLivraria.nomeLivro = "O livro";
+        LivroLivraria.emprestado = false;
+        LivroLivraria.dataEntrega = new Date();
+        LivroLivraria.emprestadoA = "Italo";
+        String statusLivro;
 
+        if (LivroLivraria.emprestado) {  // faço a inserção da String a partir do valor booleano. Através do value.of
+            statusLivro = String.valueOf(LivroLivraria.emprestado);
+            statusLivro = "Emprestado";
+        } else {
+            statusLivro = "Não emprestado";
+        }
         System.out.println("Preço do livro é: " + LivroLivraria.precoLivro);
         System.out.println("Nome do livro: " + LivroLivraria.nomeLivro);
         System.out.println("Autor: " + LivroLivraria.autor);
+        System.out.println("Status emprestado do livro: " + statusLivro);
+        System.out.println("Cliente atual: " + LivroLivraria.emprestadoA);
     }
 }
 
