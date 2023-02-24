19b9941584c7a74b3a87969acc64be1755cd7656
italo <italogithub@gmail.com>
2023-02-24 10:35:01 -0300

    add app banco

---
+++ POO/Exercicios/Ex03/AppConta.java
@@ -0,0 +1,66 @@
+package POO.Exercicios.Ex03;
+
+
+import java.util.Scanner;
+
+public class AppConta {
+
+    public static String senhaNova() {
+        Scanner scan = new Scanner(System.in);
+        String senhaNova;
+        System.out.println("Crie sua senha: ");
+        senhaNova = scan.next();
+        return senhaNova;
+    }
+
+    public static void main(String[] args) {
+        ContaCorrente conta = new ContaCorrente();
+
+        Scanner scan = new Scanner(System.in);
+        String situacaoEmprestimo;
+        String situacaoEspecial;
+
+
+        conta.senha = senhaNova();
+        conta.numConta = "12346";
+        conta.agencia = "4004";
+        conta.cvv = "156";
+        conta.emprestimoAtivo = true;
+        conta.especial = true;
+        conta.limiteEspecial = 1500.0;
+        conta.saldo = 15000;
+        boolean aproved = false;
+
+        if (conta.emprestimoAtivo) {
+            situacaoEmprestimo = String.valueOf(conta.emprestimoAtivo);
+            situacaoEmprestimo = "Empréstimo Ativo";
+        } else {
+            situacaoEmprestimo = "Empréstimo inativo";
+        }
+        if (conta.especial) {
+            situacaoEspecial = String.valueOf(conta.especial);
+            situacaoEspecial = "Conta especial ATIVA";
+        } else {
+            situacaoEspecial = "Conta especial INATIVA";
+        }
+        while (!aproved) {
+            System.out.print("Para ver os dados da sua conta, digite sua senha: ");
+            String senha = scan.next();
+
+            if (senha.equals(conta.senha)) {
+                aproved = true;
+                System.out.println("Senha aprovada!");
+                System.out.println("Número da conta: " + conta.numConta);
+                System.out.println("Agencia: " + conta.agencia);
+                System.out.println("CVV: " + conta.cvv);
+                System.out.println("Situação da conta: " + situacaoEspecial);
+                System.out.println("Situação de empréstimo: " + situacaoEmprestimo);
+                System.out.println("Limite cheque especial: " + conta.limiteEspecial);
+                System.out.println("Saldo: " + conta.saldo);
+            } else {
+                System.out.println("Senha incorreta, tente novamente!");
+            }
+        }
+    }
+}
+
---
+++ POO/Exercicios/Ex03/ContaCorrente.java
@@ -0,0 +1,19 @@
+package POO.Exercicios.Ex03;
+
+import java.util.Scanner;
+
+public class ContaCorrente {
+
+    String numConta;
+    String agencia;
+    String cvv;
+    boolean especial;
+    double limiteEspecial;
+    double saldo;
+    boolean emprestimoAtivo;
+    String senha;
+
+
+}
+
+
