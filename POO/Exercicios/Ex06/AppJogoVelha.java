95d14a8833803a5dfd98343e6e33e91b35492f8f
italo <italogithub@gmail.com>
2023-03-02 01:19:12 -0300

    update

--- POO/Exercicios/Ex06/AppJogoVelha.java
+++ POO/Exercicios/Ex06/AppJogoVelha.java
@@ -1,7 +1,11 @@
 package POO.Exercicios.Ex06;
 
+import java.util.Scanner;
+
 public class AppJogoVelha {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
 
     }
 }
--- POO/Exercicios/Ex06/JogoVelha.java
+++ POO/Exercicios/Ex06/JogoVelha.java
@@ -4,4 +4,15 @@ public class JogoVelha {
 
     char[][] jogoVelha = new char[3][3];
     int jogada = 1;
+
+
+    boolean validarJogada(int linha, int coluna, char sinal){
+        if(jogoVelha[linha][coluna] == 'X' || jogoVelha[linha][coluna] == 'O'){
+            return false;
+        }else {
+            jogoVelha[linha][coluna] = sinal;
+            jogada++;
+            return true;
+        }
+    }
 }
