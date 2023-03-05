e89ed3b8b94f5ffcd25c2aff774efd7abeb09109
italo <italogithub@gmail.com>
2023-03-05 11:51:50 -0300

    translate

--- Curso_Java/src/com/italo/exercicios/Iniciais/Exercicio12.java
+++ Curso_Java/src/com/italo/exercicios/Iniciais/Exercicio12.java
@@ -1,51 +1,50 @@
 package Curso_Java.src.com.italo.exercicios.Iniciais;
 
 import java.util.Scanner;
-
 public class Exercicio12 {
-    public static final double TAXA_INSS = 0.08;
-    public static final double TAXA_IR = 0.11;
-    public static final double TAXA_SINDICATO = 0.05;
+    public static final double INSS_TAX = 0.08;
+    public static final double IR_TAX = 0.11;
+    public static final double UNION_TAX = 0.05;
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("Digite quanto você recebe por hora: ");
-        double valorHora = scan.nextDouble();
+        System.out.print("Enter how much you earn per hour: ");
+        double hourlyRate = scan.nextDouble();
 
-        System.out.print("Digite a quantidade de horas trabalhadas no mês: ");
-        double horas = scan.nextDouble();
+        System.out.print("Enter the amount of hours worked in the month: ");
+        double hoursWorked = scan.nextDouble();
 
-        double salarioBruto = calcularSalarioBruto(valorHora, horas);
-        double totalDescontos = calcularDescontos(salarioBruto);
-        double salarioLiquido = salarioBruto - totalDescontos;
+        double grossSalary = calculateGrossSalary(hourlyRate, hoursWorked);
+        double totalDeductions = calculateDeductions(grossSalary);
+        double netSalary = grossSalary - totalDeductions;
 
         System.out.println("=====================================================");
-        System.out.printf("Seu salário bruto foi de: %.2f R$\n", salarioBruto);
-        System.out.printf("Desconto do imposto de renda: %.2f R$\n", calcularImpostoRenda(salarioBruto));
-        System.out.printf("Desconto do INSS: %.2f R$\n", calcularINSS(salarioBruto));
-        System.out.printf("Desconto do Sindicato: %.2f R$\n", calcularSindicato(salarioBruto));
-        System.out.printf("Seu saldo liquido: %.2f R$\n", salarioLiquido);
+        System.out.printf("Your gross salary was: %.2f R$\n", grossSalary);
+        System.out.printf("Income tax deduction: %.2f R$\n", calculateIncomeTax(grossSalary));
+        System.out.printf("INSS deduction: %.2f R$\n", calculateINSS(grossSalary));
+        System.out.printf("Union tax deduction: %.2f R$\n", calculateUnionTax(grossSalary));
+        System.out.printf("Your net salary: %.2f R$\n", netSalary);
         System.out.println("======================================================");
     }
 
-    public static double calcularSalarioBruto(double valorHora, double horas) {
-        return valorHora * horas;
+    public static double calculateGrossSalary(double hourlyRate, double hoursWorked) {
+        return hourlyRate * hoursWorked;
     }
 
-    public static double calcularINSS(double salarioBruto) {
-        return salarioBruto * TAXA_INSS;
+    public static double calculateINSS(double grossSalary) {
+        return grossSalary * INSS_TAX;
     }
 
-    public static double calcularImpostoRenda(double salarioBruto) {
-        return salarioBruto * TAXA_IR;
+    public static double calculateIncomeTax(double grossSalary) {
+        return grossSalary * IR_TAX;
     }
 
-    public static double calcularSindicato(double salarioBruto) {
-        return salarioBruto * TAXA_SINDICATO;
+    public static double calculateUnionTax(double grossSalary) {
+        return grossSalary * UNION_TAX;
     }
 
-    public static double calcularDescontos(double salarioBruto) {
-        return calcularINSS(salarioBruto) + calcularImpostoRenda(salarioBruto) + calcularSindicato(salarioBruto);
+    public static double calculateDeductions(double grossSalary) {
+        return calculateINSS(grossSalary) + calculateIncomeTax(grossSalary) + calculateUnionTax(grossSalary);
     }
 }
