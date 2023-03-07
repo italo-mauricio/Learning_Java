6ede193ca8f0a330a745e1c072d3f7778869b82d
italo <italogithub@gmail.com>
2023-03-07 17:17:37 -0300

    adicionando aula sobre sobrecarga de construtores e métodos

---
+++ POO/com/italo/aulas/SobrecargaConstrutores.java
@@ -0,0 +1,38 @@
+package POO.com.italo.aulas;
+
+public class SobrecargaConstrutores {
+
+    /***
+     *  public Carro(String marca, String modelo, int numPassageiros,
+     *      double capCombustivel, double consumoCombustivel) {
+     *          this.marca = marca;
+     *          this.modelo = modelo;
+     *          this.numPassageiros = numPassageiros;
+     *          this.capCombustivel = capCombustivel;
+     *          this.consumoCombustivel = consumoCombustivel;
+     *      }
+     *
+     *      public Carro(){}
+     *
+     *      public Carro(String marca, String modelo, int numPassageiros){
+     *          this.marca = marca;
+     *          this.modelo = modelo;
+     *          this.numPssageiros = numPassageiros;
+     *          System.out.println("Chamando o construtor com 3 parâmetros");
+     *      }
+     *
+     *      public Carro(String marca, String modelo){
+     *          this(marca, modelo, 10);
+     *          System.out.println("Chamando o construtor com 2 parâmeetros");
+     *      }
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     */
+}
---
+++ POO/com/italo/aulas/SobrecargaMetodo.java
@@ -0,0 +1,28 @@
+package POO.com.italo.aulas;
+
+public class SobrecargaMetodo {
+    /***
+     * A sobrecarga de um método consiste em declarar um ou mais métodos com o mesmo nome
+     *
+     * Exemplo:
+     *
+     *             public class MinhaCalculadora{
+     *                  public int soma(int num1, int num2){
+     *                      retrun num1 + num2;
+     *                  }
+     *                  public int soma(int num1, int num2, int num3){
+     *                      return num1 + num2 + num3;
+     *                  }
+     *                  public double soma(double num1, double num2){
+     *                      return num1 + num2;
+     *                  }
+     *              }
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     */
+}
