2c67c92710c929a8a6af3930bcb5712de1836b30
italo <italogithub@gmail.com>
2023-03-15 20:18:33 -0300

    add exercicio 12

---
+++ POO/com/italo/exercicios/Ex12/Lampada2.java
@@ -0,0 +1,88 @@
+package POO.com.italo.exercicios.Ex12;
+
+public class Lampada2 {
+    private String modelo;
+    private String tensao;
+    private int potencia;
+    private String corLuz;
+    private String cor;
+    private int garantiaMeses;
+    private String [] tipos;
+    private boolean tipoAbajur;
+    private boolean ligado;
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getTensao() {
+        return tensao;
+    }
+
+    public void setTensao(String tensao) {
+        this.tensao = tensao;
+    }
+
+    public int getPotencia() {
+        return potencia;
+    }
+
+    public void setPotencia(int potencia) {
+        this.potencia = potencia;
+    }
+
+    public String getCorLuz() {
+        return corLuz;
+    }
+
+    public void setCorLuz(String corLuz) {
+        this.corLuz = corLuz;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getGarantiaMeses() {
+        return garantiaMeses;
+    }
+
+    public void setGarantiaMeses(int garantiaMeses) {
+        this.garantiaMeses = garantiaMeses;
+    }
+
+    public String[] getTipos() {
+        return tipos;
+    }
+
+    public void setTipos(String[] tipos) {
+        this.tipos = tipos;
+    }
+
+    public boolean isTipoAbajur() {
+        return tipoAbajur;
+    }
+
+    public void setTipoAbajur(boolean tipoAbajur) {
+        this.tipoAbajur = tipoAbajur;
+    }
+
+    public boolean isLigado() {
+        return ligado;
+    }
+
+    public void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
+
+
+
+}
