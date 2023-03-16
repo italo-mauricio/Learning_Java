f047754022dd543112eb472708fca2bf8acd8ed3
italo <italogithub@gmail.com>
2023-03-16 13:27:04 -0300

    add comentário

--- POO/com/italo/exercicios/Ex13/AppBanco.java
+++ POO/com/italo/exercicios/Ex13/AppBanco.java
@@ -1,6 +1,12 @@
 package POO.com.italo.exercicios.Ex13;
 
-import POO.com.italo.exercicios.Ex03.ContaCorrente;
+
+/***
+ * Crie uma classe para representar uma conta corrente que possui um número, um saldo, um status que informa
+ * se ela é especial ou não, um limite. Desenvolva métodos para realizar saque (verificando se o cliente pode realizar saques).
+ * depositar dinheiro, consultar saldo e verificar se o cliente está usando cheque especial ou não. Desenvolva um programa para testar
+ * essa classe.
+ */
 
 import java.util.Scanner;
 
