0bc1dc99807ec67415b1fd7f7044041b206b4ec9
italo <italogithub@gmail.com>
2023-03-19 13:39:36 -0300

    exemplificando métodos static

--- POO/com/italo/aulas/MetodosEstaticos/AppCalculadora.java
+++ POO/com/italo/aulas/MetodosEstaticos/AppCalculadora.java
@@ -1,10 +1,13 @@
 package POO.com.italo.aulas.MetodosEstaticos;
 
 public class AppCalculadora {
+    static int resultSoma = 0;  // aqui exemplifica como o método estatico funciona
     public static void main(String[] args) {
-        Calculadora calcula = new Calculadora();
 
-        calcula.soma(1, 14);
-        calcula.soma(1.3, 1.4);
+
+        // retiro a necessidade de instanciar a classe
+       resultSoma = Calculadora.soma(2, 3);
+       System.out.println("Resultado da soma é: " + resultSoma);
+
     }
 }
--- POO/com/italo/aulas/MetodosEstaticos/Calculadora.java
+++ POO/com/italo/aulas/MetodosEstaticos/Calculadora.java
@@ -1,16 +1,16 @@
 package POO.com.italo.aulas.MetodosEstaticos;
 
-public class Calculadora {
-    public int soma(int num1, int num2){
+public class Calculadora {  // é chamado de classe utilitária
+    public static int soma(int num1, int num2){
         return num1 + num2;
     }
-    public double soma(double num1, double num2){
+    public static double soma(double num1, double num2){
         return num1 + num2;
     }
-    public int soma(int num1, int num2, int num3){
+    public static int soma(int num1, int num2, int num3){
         return num1 + num2 + num3;
     }
-    public int soma(int[] vetorInteiros){
+    public static int soma(int[] vetorInteiros){
         int total = 0;
 
         for(int i = 0; i < vetorInteiros.length; i++){
