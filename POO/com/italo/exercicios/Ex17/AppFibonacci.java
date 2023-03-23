4443ab122ba7ee250c14e043cf3fac11bd3ae7e6
italo <italogithub@gmail.com>
2023-03-23 19:06:16 -0300

    melhorando fibonacci

--- POO/com/italo/exercicios/Ex17/AppFibonacci.java
+++ POO/com/italo/exercicios/Ex17/AppFibonacci.java
@@ -1,7 +1,17 @@
 package POO.com.italo.exercicios.Ex17;
 
+import java.util.Scanner;
+
 public class AppFibonacci {
     public static void main(String[] args) {
-        System.out.println(Fibonacci.fibonacci((10)));
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+        int num = scan.nextInt();
+        for (int i = 0; i < num; i++) {
+            System.out.println(Fibonacci.fibonacci(i) + " ");
+        }
+
+
     }
 }
