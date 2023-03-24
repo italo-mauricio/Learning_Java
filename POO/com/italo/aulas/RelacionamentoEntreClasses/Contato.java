d7e525f3d174ff11464d0e9a3a5345e986c99b8d
italo <italogithub@gmail.com>
2023-03-24 15:20:36 -0300

    utilizando a classe endereço na classe contato

--- POO/com/italo/aulas/RelacionamentoEntreClasses/Contato.java
+++ POO/com/italo/aulas/RelacionamentoEntreClasses/Contato.java
@@ -2,7 +2,7 @@ package POO.com.italo.aulas.RelacionamentoEntreClasses;
 
 public class Contato {
     private String nome;
-    private String endereco;
+    private Endereco endereco;
     private String telefone;
 
     public String getNome() {
@@ -13,11 +13,11 @@ public class Contato {
         this.nome = nome;
     }
 
-    public String getEndereco() {
+    public Endereco getEndereco() {
         return endereco;
     }
 
-    public void setEndereco(String endereco) {
+    public void setEndereco(Endereco endereco) {
         this.endereco = endereco;
     }
 
---
+++ POO/com/italo/aulas/RelacionamentoEntreClasses/Endereco.java
@@ -0,0 +1,58 @@
+package POO.com.italo.aulas.RelacionamentoEntreClasses;
+
+public class Endereco {
+    private String nomeRua;
+    private String complemento;
+    private String cidade;
+    private String estado;
+    private String cep;
+    private String numero;
+
+    public String getNomeRua() {
+        return nomeRua;
+    }
+
+    public void setNomeRua(String nomeRua) {
+        this.nomeRua = nomeRua;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+}
