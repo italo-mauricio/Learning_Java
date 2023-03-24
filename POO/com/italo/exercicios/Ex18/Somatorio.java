77ec13065b5d443345f1997b54aaf17c9d419657
italo <italogithub@gmail.com>
2023-03-24 01:49:38 -0300

    add exercicio 18

---
+++ POO/com/italo/exercicios/Ex18/Somatorio.java
@@ -0,0 +1,16 @@
+package POO.com.italo.exercicios.Ex18;
+
+public class Somatorio {
+
+    public static int fibonacci(int num){
+
+        if(num < 2){
+            return 1;
+        }
+        return fibonacci(num-1) + fibonacci((num -2));
+    }
+    public static int somatorio(int num){
+        return num + somatorio(num -1);
+    }
+}
+
