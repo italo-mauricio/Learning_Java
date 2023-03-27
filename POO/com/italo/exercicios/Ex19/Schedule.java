b3b44e17789207f6b8e606a8f84f4635d836e47e
italo <italogithub@gmail.com>
2023-03-26 23:27:34 -0300

    add schedule exercise

--- POO/com/italo/exercicios/Ex19/Schedule.java
+++ POO/com/italo/exercicios/Ex19/Schedule.java
@@ -2,6 +2,33 @@ package POO.com.italo.exercicios.Ex19;
 
 public class Schedule {
     private String name;
+    private Contact[] contacts;
+    private Address addres;
+    private Phone cellPhone;
+
+    public Phone getCellPhone() {
+        return cellPhone;
+    }
+
+    public void setCellPhone(Phone cellPhone) {
+        this.cellPhone = cellPhone;
+    }
+
+    public Address getAddres() {
+        return addres;
+    }
+
+    public void setAddres(Address addres) {
+        this.addres = addres;
+    }
+
+    public Contact[] getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(Contact[] contacts) {
+        this.contacts = contacts;
+    }
 
     public String getName() {
         return name;
