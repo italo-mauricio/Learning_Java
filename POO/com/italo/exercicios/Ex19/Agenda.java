727db418ee173200392edff015c5741a82418fcd
italo <italogithub@gmail.com>
2023-03-26 21:49:05 -0300

    adicionando exercicio agenda

---
+++ POO/com/italo/exercicios/Ex19/Agenda.java
@@ -0,0 +1,8 @@
+package POO.com.italo.exercicios.Ex19;
+
+public class Agenda {
+    private String nome;
+    private String email;
+    private String numero;
+    private Endereco endereco;
+}
---
+++ POO/com/italo/exercicios/Ex19/AppAgenda.java
@@ -0,0 +1,11 @@
+package POO.com.italo.exercicios.Ex19;
+
+import java.util.Scanner;
+
+public class AppAgenda {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        Agenda
+    }
+}
