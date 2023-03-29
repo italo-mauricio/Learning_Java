17fa2279447c75d66995fe24600faad049989830
italo <italogithub@gmail.com>
2023-03-29 10:56:59 -0300

    add getters and setters

--- POO/com/italo/exercicios/Ex20/Course.java
+++ POO/com/italo/exercicios/Ex20/Course.java
@@ -4,4 +4,37 @@ public class Course {
     private String nameCourse;
     private String hour;
     private Teacher teacher;
+    private Student[] students;
+
+    public String getNameCourse() {
+        return nameCourse;
+    }
+
+    public void setNameCourse(String nameCourse) {
+        this.nameCourse = nameCourse;
+    }
+
+    public String getHour() {
+        return hour;
+    }
+
+    public void setHour(String hour) {
+        this.hour = hour;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public void setStudents(Student[] students) {
+        this.students = students;
+    }
 }
