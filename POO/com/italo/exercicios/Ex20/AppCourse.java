d7cedca56366972aa6ede96c6a5b1631bfee2d87
italo <italogithub@gmail.com>
2023-03-29 11:13:54 -0300

    set setting the class teacher and course

--- POO/com/italo/exercicios/Ex20/AppCourse.java
+++ POO/com/italo/exercicios/Ex20/AppCourse.java
@@ -10,5 +10,20 @@ public class AppCourse {
         String nameCourse = scan.nextLine();
         System.out.println("Enter course time: ");
         String timeCourse = scan.nextLine();
+        System.out.println("Enter the teacher's name: ");
+        String nameTeacher = scan.nextLine();
+        System.out.println("Enter the teacher's email: ");
+        String depEmail = scan.nextLine();
+        System.out.println("Enter the teacher's department: ");
+        String depDepartment = scan.nextLine();
+
+        Course course = new Course();
+        course.setNameCourse(nameCourse);
+        course.setTime(timeCourse);
+
+        Teacher teacher = new Teacher();
+        teacher.setNameTeacher(nameTeacher);
+        teacher.setEmail(depEmail);
+        teacher.setDepartment(depDepartment);
     }
 }
