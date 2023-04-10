2e6956e5fecffb570201d5c00bf6ef1c8de3c3d8
italo <italogithub@gmail.com>
2023-04-10 09:52:43 -0300

    feat: update

--- POO/com/italo/aulas/Heranca/Teacher.java
+++ POO/com/italo/aulas/Heranca/Teacher.java
@@ -5,4 +5,54 @@ public class Teacher {
     private String address;
     private String phone;
     private String cpf;
+    private double salary;
+    private String nameCourse;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getNameCourse() {
+        return nameCourse;
+    }
+
+    public void setNameCourse(String nameCourse) {
+        this.nameCourse = nameCourse;
+    }
 }
