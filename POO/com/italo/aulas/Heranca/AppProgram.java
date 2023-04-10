5ba745930b258d5292d69a9af17d1e9dc6c66811
italo <italogithub@gmail.com>
2023-04-10 12:36:12 -0300

    feat: create people class

---
+++ POO/com/italo/aulas/Heranca/AppProgram.java
@@ -0,0 +1,15 @@
+package POO.com.italo.aulas.Heranca;
+
+public class AppProgram {
+    public static void main(String[] args) {
+        Student student = new Student();
+        Teacher teacher = new Teacher();
+
+        People people = new People();
+
+        student.setName("italo");
+        System.out.println(student.getName());
+        teacher.setPrivateNumber("1232132121");
+        System.out.println(teacher.getPrivateNumber());
+;    }
+}
--- POO/com/italo/aulas/Heranca/Teacher.java
+++ POO/com/italo/aulas/Heranca/Teacher.java
@@ -1,44 +1,10 @@
 package POO.com.italo.aulas.Heranca;
 
-public class Teacher {
-    private String name;
-    private String address;
-    private String phone;
-    private String cpf;
+public class Teacher extends People{
+
     private double salary;
     private String nameCourse;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
 
     public double getSalary() {
         return salary;
