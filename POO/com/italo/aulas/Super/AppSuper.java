67de7306d25200b57b811281b662ee0383698362
italo <italogithub@gmail.com>
2023-04-15 22:30:23 -0300

    using super class

---
+++ POO/com/italo/aulas/Super/AppSuper.java
@@ -0,0 +1,7 @@
+package POO.com.italo.aulas.Super;
+
+public class AppSuper {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ POO/com/italo/aulas/Super/Student.java
@@ -0,0 +1,40 @@
+package POO.com.italo.aulas.Super;
+
+public class Student extends People {
+
+    private String course;
+    private double[] notes;
+
+    public Student(){
+        super();
+    }
+
+    public Student(String name, String address, String phone, String course) {
+        super(name, address, phone);
+        this.course = course;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public double[] getNotes() {
+        return notes;
+    }
+
+    public void setNotes(double[] notes) {
+        this.notes = notes;
+    }
+
+    public double calculateAverage(){
+        return 0;
+    }
+
+    public boolean chekAproved(){
+        return true;
+    }
+}
