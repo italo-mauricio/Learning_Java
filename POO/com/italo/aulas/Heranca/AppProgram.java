a7d8bcb3dab37be292eb771ce66b0c305f63bb3a
italo <italogithub@gmail.com>
2023-04-15 22:07:28 -0300

    update herança

--- POO/com/italo/aulas/Heranca/AppProgram.java
+++ POO/com/italo/aulas/Heranca/AppProgram.java
@@ -2,14 +2,12 @@ package POO.com.italo.aulas.Heranca;
 
 public class AppProgram {
     public static void main(String[] args) {
-        Student student = new Student();
-        Teacher teacher = new Teacher();
-        People people = new People();
+        People student = new Student(); // posso chamar a classe Pai ou Mãe e declarar a classe filha com todos os atributos
 
-        student.setName("italo");
-        System.out.println(student.getName());
-        teacher.setPrivateNumber("1232132121");
-        System.out.println(teacher.getPrivateNumber());
-
+        /*
+        * Lembrete: não faça reuso de herança para classes sem sentido lógico entre elas. Por exemplo, não reutilize tipo
+        * uma classe cachorro que contem nome e endereço, com uma classe pessoa que também contém nome e endereço, é uma má prática.
+        *
+        * */
 ;    }
 }
