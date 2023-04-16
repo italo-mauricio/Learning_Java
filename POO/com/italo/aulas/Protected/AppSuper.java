eb25c7101a7497c246c935832b25110e06a2e1ad
italo <italogithub@gmail.com>
2023-04-15 23:48:38 -0300

    learning acess protectd

---
+++ POO/com/italo/aulas/Protected/AppSuper.java
@@ -0,0 +1,7 @@
+package POO.com.italo.aulas.Protected;
+
+public class AppSuper {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ POO/com/italo/aulas/Protected/Teacher.java
@@ -0,0 +1,28 @@
+package POO.com.italo.aulas.Protected;
+
+public class Teacher extends People {
+
+    private double salary;
+    private String nameCourse;
+
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getNameCourse() {
+        return nameCourse;
+    }
+
+    public void setNameCourse(String nameCourse) {
+        this.nameCourse = nameCourse;
+    }
+
+    public double calculateLiquidSalary(){
+        return 0;
+    }
+}
