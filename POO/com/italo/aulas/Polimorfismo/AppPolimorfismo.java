a6d2b8434c312837be2d6df2f68b2bea4a75bcfc
italo <italogithub@gmail.com>
2023-04-18 18:26:13 -0300

    feat: learning polimorfismo

---
+++ POO/com/italo/aulas/Polimorfismo/AppPolimorfismo.java
@@ -0,0 +1,7 @@
+package POO.com.italo.aulas.Polimorfismo;
+
+public class AppPolimorfismo {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ POO/com/italo/aulas/Polimorfismo/People.java
@@ -0,0 +1,58 @@
+package POO.com.italo.aulas.Polimorfismo;
+
+public class People {
+    private String name;
+    private String address;
+    private String phone;
+    private String cpf;
+    private String privateNumber;
+    protected String nomeVisibilidade;
+    public People(){
+    }
+    public People(String name, String address, String phone) {
+        super();
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public String getPrivateNumber() {
+        return privateNumber;
+    }
+
+    public void setPrivateNumber(String privateNumber) {
+        this.privateNumber = privateNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
