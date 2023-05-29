8404724eaece6d83606a571fea53cb84435a1a07
italo <italogithub@gmail.com>
2023-05-28 23:58:55 -0300

    update

--- POO/com/italo/desafiosPOO/conta/AppBank.java
+++ POO/com/italo/desafiosPOO/conta/AppBank.java
@@ -7,7 +7,17 @@ public class AppBank {
         AccountBank accountSimple = new AccountBank();
         accountSimple.setClientName("Client simple account");
         accountSimple.setAccountNumber("12345");
+        accountSimple.deposit(100);
+        withdrawFunds(accountSimple, 50);
+        withdrawFunds(accountSimple, 70);
 
         System.out.println(accountSimple);
+    }
+    private static void withdrawFunds(AccountBank accountBank, double value){
+        if (accountBank.withdraw(value)){
+            System.out.println("Withdraw Sucessfully, new balance: " + accountBank.getCurrency());
+        } else {
+            System.out.println("Insufficient funds for withdrawal: " + value + " current balance: " + accountBank.getCurrency());
+        }
     }
 }
