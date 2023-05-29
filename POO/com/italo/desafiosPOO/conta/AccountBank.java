39167df590a525083fba96b8356cc6e94ac13bed
italo <italogithub@gmail.com>
2023-05-28 23:01:58 -0300

    iniciando estudos mais avançados de POO

---
+++ POO/com/italo/desafiosPOO/conta/AccountBank.java
@@ -0,0 +1,50 @@
+package POO.com.italo.desafiosPOO.conta;
+
+public class AccountBank {
+
+    private String clientName;
+    private String accountNumber;
+    private double currency;
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(double currency) {
+        this.currency = currency;
+    }
+
+
+    public void deposit(double value){
+        currency += value;
+    }
+
+    public boolean withdraw(double value){
+        if((currency - value) >= 0){
+            currency -= value;
+            return true;
+        }
+        return false;
+    }
+}
---
+++ POO/com/italo/desafiosPOO/conta/AppBank.java
@@ -0,0 +1,5 @@
+package POO.com.italo.desafiosPOO.conta;
+
+public class AppBank {
+
+}
