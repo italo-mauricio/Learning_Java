0b5e1516cfa453356fac3fa88406037ecc5568d6
italo <italomauricio98@gmail.com>
2023-06-25 19:33:10 -0300

    update

---
+++ Exception/Erro1.java
@@ -0,0 +1,10 @@
+package Exception;
+
+public class Erro1 {
+    public static void main(String[] args) {
+        int[] vetor = new int[4];
+        System.out.println("Antes da exception");
+
+
+    }
+}
